7ed4e9247e55f13371f493239116b4f69661c240
Vidith Agarwal <vidithagarwal315@gmail.com>
2024-10-03 22:44:41 -0400

    Added CSS and Bootstrap for A2

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -3,111 +3,155 @@ export default function Dashboard() {
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
       <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
-      <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <img src="/images/reactjs.png" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
-              href="#/Kanbas/Courses/1234/Home">
-              CS1234 React JS
-            </a>
-            <p className="wd-dashboard-course-title">
-              Full Stack software developer
-            </p>
-            <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+      <div id="wd-dashboard-courses" className="row">
+        <div className="row row-cols-1 row-cols-md-5 g-4">
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card">
+              <img src="/images/react.jpg" />
+              <div className="card-body">
+                <a
+                  className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/1234/Home"
+                  style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}
+                >
+                  CS1234 React JS
+                </a>
+                <p className="wd-dashboard-course-title">
+                  Full Stack software developer
+                </p>
+                <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="wd-dashboard-course">
-          <img src="/images/testing.jpg" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
-              href="#/Kanbas/Courses/1234/Home">
-              CS1234 Testing JS
-            </a>
-            <p className="wd-dashboard-course-title">
-              Full Stack software developer
-            </p>
-            <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card">
+              <img src="/images/test.jpg" />
+              <div className="card-body">
+                <a
+                  className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/1234/Home"
+                  style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}
+                >
+                  CS1234 Testing JS
+                </a>
+                <p className="wd-dashboard-course-title">
+                  Full Stack software developer
+                </p>
+                <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="wd-dashboard-course">
-          <img src="/images/CICD.png" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
-              href="#/Kanbas/Courses/1234/Home">
-              CS1234 CiCD
-            </a>
-            <p className="wd-dashboard-course-title">
-              Full Stack software developer
-            </p>
-            <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card">
+              <img src="/images/cicd.jpg" />
+              <div className="card-body">
+                <a
+                  className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/1234/Home"
+                  style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}
+                >
+                  CS1234 CiCD
+                </a>
+                <p className="wd-dashboard-course-title">
+                  Full Stack software developer
+                </p>
+                <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="wd-dashboard-course">
-          <img src="/images/typescript.jpg" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
-              href="#/Kanbas/Courses/1234/Home">
-              CS1234 Typescript JS
-            </a>
-            <p className="wd-dashboard-course-title">
-              Full Stack software developer
-            </p>
-            <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card">
+              <img src="/images/java.jpg" />
+              <div className="card-body">
+                <a
+                  className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/1234/Home"
+                  style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}
+                >
+                  CS1234 Java
+                </a>
+                <p className="wd-dashboard-course-title">
+                  Full Stack software developer
+                </p>
+                <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="wd-dashboard-course">
-          <img src="/images/java.jpeg" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
-              href="#/Kanbas/Courses/1234/Home">
-              CS1234 Java
-            </a>
-            <p className="wd-dashboard-course-title">
-              Full Stack software developer
-            </p>
-            <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card">
+              <img src="/images/html.jpg" />
+              <div className="card-body">
+                <a
+                  className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/1234/Home"
+                  style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}
+                >
+                  CS1234 Html JS
+                </a>
+                <p className="wd-dashboard-course-title">
+                  Full Stack software developer
+                </p>
+                <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="wd-dashboard-course">
-          <img src="/images/html.jpeg" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
-              href="#/Kanbas/Courses/1234/Home">
-              CS1234 Html
-            </a>
-            <p className="wd-dashboard-course-title">
-              Full Stack software developer
-            </p>
-            <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card">
+              <img src="/images/css.jpg" />
+              <div className="card-body">
+                <a
+                  className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/1234/Home"
+                  style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}
+                >
+                  CS1234 CSS JS
+                </a>
+                <p className="wd-dashboard-course-title">
+                  Full Stack software developer
+                </p>
+                <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="wd-dashboard-course">
-          <img src="/images/css.jpeg" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
-              href="#/Kanbas/Courses/1234/Home">
-              CS1234 CSS
-            </a>
-            <p className="wd-dashboard-course-title">
-              Full Stack software developer
-            </p>
-            <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card">
+              <img src="/images/js.jpg" />
+              <div className="card-body">
+                <a
+                  className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/1234/Home"
+                  style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}
+                >
+                  CS1234 JS
+                </a>
+                <p className="wd-dashboard-course-title">
+                  Full Stack software developer
+                </p>
+                <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="wd-dashboard-course">
-          <img src="/images/javascript.png" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
-              href="#/Kanbas/Courses/1234/Home">
-              CS1234 JS
-            </a>
-            <p className="wd-dashboard-course-title">
-              Full Stack software developer
-            </p>
-            <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card">
+              <img src="/images/react.jpg" />
+              <div className="card-body">
+                <a
+                  className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/1234/Home"
+                  style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}
+                >
+                  CS1234 React JS
+                </a>
+                <p className="wd-dashboard-course-title">
+                  Full Stack software developer
+                </p>
+                <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-);}
+  );
+}
--- src/Kanbas/Navigation.jsx
+++ src/Kanbas/Navigation.jsx
@@ -1,13 +1,19 @@
-import { Link } from "react-router-dom";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { IoCalendarOutline } from "react-icons/io5";
+import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
+import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
+import { IoChevronBackCircle } from "react-icons/io5";
 export default function KanbasNavigation() {
-  return (
-    <div id="wd-kanbas-navigation">
-      <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a><br/>
-      <Link to="/Kanbas/Account" id="wd-account-link">Account</Link><br/>
-      <Link to="/Kanbas/Dashboard" id="wd-dashboard-link">Dashboard</Link><br/>
-      <Link to="/Kanbas/Dashboard" id="wd-course-link">Courses</Link><br/>
-      <Link to="/Kanbas/Calendar" id="wd-calendar-link">Calendar</Link><br/>
-      <Link to="/Kanbas/Inbox" id="wd-inbox-link">Inbox</Link><br/>
-      <Link to="/Labs" id="wd-labs-link">Labs</Link><br/>
+    return (
+      <div id="wd-kanbas-navigation" className="list-group rounded-0 d-flex flex-column justify-content-center align-items-center">
+      <a id="wd-neu-link" target="_blank" href="https://www.northeastern.edu/" className="list-group-item bg-black border-0"><img src="/images/neu.webp" width="75px" /></a>
+      <a id="wd-account-link" href="#/Kanbas/Account" className="list-group-item text-white bg-black text-center border-0"><FaRegCircleUser className="fs-1 text text-white" /><br />Account</a>
+      <a id="wd-dashboard-link" href="#/Kanbas/Dashboard" className="list-group-item text-center border-0 bg-white text-danger"><AiOutlineDashboard className="fs-1 text-danger" /><br />Dashboard</a>
+      <a id="wd-course-link" href="#/Kanbas/Courses" className="list-group-item text-white bg-black text-center border-0"><LiaBookSolid className="fs-1 text-danger" /><br />Courses</a>
+      <a id="wd-calendar-link" href="#/Kanbas/Calendar" className="list-group-item text-white bg-black text-center border-0"><IoCalendarOutline className="fs-1 text-danger" /><br />Calendar</a>
+      <a id="wd-inbox-link" href="#/Kanbas/Inbox" className="list-group-item text-white bg-black text-center border-0"><FaInbox className="fs-1 text-danger" /><br />Inbox</a>
+      <a id="wd-labs-link" href="#/Labs" className="list-group-item text-white bg-black text-center border-0"><LiaCogSolid className="fs-1 text-danger" /><br />Labs</a>
+      <a id="wd-landing-page" className="list-group-item text-white bg-black text-center border-0" href="/"><IoChevronBackCircle  className="fs-1 text-danger"/><br />Back to landing page</a>
     </div>
-);}
+  );}
+  
